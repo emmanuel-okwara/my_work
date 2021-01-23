289b98357f51e5a538413423ca1a6c78a5efb9a9
unknown <okwara.ec@gmail.com>
2021-01-23 12:25:43 +0200

    sys_module_py

---
+++ python_learning_course/sys_module.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdout.write('hello_world\n')
+sys.stderr.write('This is an error code\n')
+sys.stderr.flush()
+dine = ''
+
+hello = sys.argv
+print(hello)
+for i in hello:
+        print(i)
+

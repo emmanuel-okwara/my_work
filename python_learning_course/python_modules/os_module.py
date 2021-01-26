de13276032f596e915f79a82ba9c213ff817fb4a
unknown <okwara.ec@gmail.com>
2021-01-26 10:50:58 +0200

    python_modules

---
+++ python_learning_course/python_modules/os_module.py
@@ -0,0 +1,32 @@
+import os 
+
+
+#rint(dir(os))
+'''print(os.getcwd())
+os.chdir('/Users/4IR/Downloads')
+print(os.getcwd())
+#print(os.listdir())
+
+#os.mkdir('You_SUCK')
+#print(os.listdir())
+#os.makedirs('os_mk/hell/yed')
+#print(os.listdir())
+
+
+#os.chdir('./os_mk/hell/yed')
+os.chdir('/Users/4IR/Downloads')
+#os.removedirs('./os_mk/hell/yed')
+print(os.getcwdb())
+print(os.listdir())
+print(os.)
+'''
+'''for dirpath ,dirname,filename in os.walk('/Users/4IR/Pictures/'):
+    print('Current Path:',dirpath)
+    print('Directories',dirname)
+    print('files',filename)
+    print('\n\n')
+'''
+x = dir(os)
+print(x)
+
+

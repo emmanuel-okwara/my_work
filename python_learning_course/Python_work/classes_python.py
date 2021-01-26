f6d19f6f368566a5a0474f268405eefc8533482c
unknown <okwara.ec@gmail.com>
2021-01-26 12:24:43 +0200

    classes_python

---
+++ python_learning_course/Python_work/classes_python.py
@@ -0,0 +1,45 @@
+#when you define an attribute with __before its name the attribute can only be called from within the clsas and no where else
+
+class Stack:
+    def __init__(self):
+        self.__stackList = []
+
+    def push(self, val):
+        self.__stackList.append(val)
+
+    def pop(self):
+        val = self.__stackList[-1]
+        del self.__stackList[-1]
+        return val
+
+
+stackObject = Stack()
+
+stackObject.push(3)
+stackObject.push(2)
+stackObject.push(1)
+
+print(stackObject.pop())
+print(stackObject.pop())
+print(stackObject.pop())class Stack:
+    def __init__(self):
+        self.__stackList = []
+
+    def push(self, val):
+        self.__stackList.append(val)
+
+    def pop(self):
+        val = self.__stackList[-1]
+        del self.__stackList[-1]
+        return val
+
+
+stackObject = Stack()
+
+stackObject.push(3)
+stackObject.push(2)
+stackObject.push(1)
+
+print(stackObject.pop())
+print(stackObject.pop())
+print(stackObject.pop())
